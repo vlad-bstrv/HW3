d8769a1163895a665f05a6773e25507a00263e55
vlad-bstrv <keocera@icloud.com>
2022-07-10 11:12:44 +0300

    Добавил методы onAttach и onDetach в презентеры

--- app/src/main/java/com/geekbrains/tests/presenter/search/SearchPresenter.kt
+++ app/src/main/java/com/geekbrains/tests/presenter/search/SearchPresenter.kt
@@ -3,6 +3,7 @@ package com.geekbrains.tests.presenter.search
 import com.geekbrains.tests.model.SearchResponse
 import com.geekbrains.tests.repository.GitHubRepository
 import com.geekbrains.tests.repository.GitHubRepository.GitHubRepositoryCallback
+import com.geekbrains.tests.view.ViewContract
 import com.geekbrains.tests.view.search.ViewSearchContract
 import retrofit2.Response
 
@@ -15,36 +16,45 @@ import retrofit2.Response
  */
 
 internal class SearchPresenter internal constructor(
-    private val viewContract: ViewSearchContract,
     private val repository: GitHubRepository
 ) : PresenterSearchContract, GitHubRepositoryCallback {
 
+    private var view: ViewSearchContract? = null
+
     override fun searchGitHub(searchQuery: String) {
-        viewContract.displayLoading(true)
+        view?.displayLoading(true)
         repository.searchGithub(searchQuery, this)
     }
 
+    override fun onAttach(view: ViewContract) {
+        this.view = view as ViewSearchContract
+    }
+
+    override fun onDetach() {
+        view = null
+    }
+
     override fun handleGitHubResponse(response: Response<SearchResponse?>?) {
-        viewContract.displayLoading(false)
+        view?.displayLoading(false)
         if (response != null && response.isSuccessful) {
             val searchResponse = response.body()
             val searchResults = searchResponse?.searchResults
             val totalCount = searchResponse?.totalCount
             if (searchResults != null && totalCount != null) {
-                viewContract.displaySearchResults(
+                view?.displaySearchResults(
                     searchResults,
                     totalCount
                 )
             } else {
-                viewContract.displayError("Search results or total count are null")
+                view?.displayError("Search results or total count are null")
             }
         } else {
-            viewContract.displayError("Response is null or unsuccessful")
+            view?.displayError("Response is null or unsuccessful")
         }
     }
 
     override fun handleGitHubError() {
-        viewContract.displayLoading(false)
-        viewContract.displayError()
+        view?.displayLoading(false)
+        view?.displayError()
     }
 }
--- app/src/main/java/com/geekbrains/tests/view/search/MainActivity.kt
+++ app/src/main/java/com/geekbrains/tests/view/search/MainActivity.kt
@@ -20,13 +20,19 @@ import retrofit2.converter.gson.GsonConverterFactory
 class MainActivity : AppCompatActivity(), ViewSearchContract {
 
     private val adapter = SearchResultAdapter()
-    private val presenter: PresenterSearchContract = SearchPresenter(this, createRepository())
+    private val presenter: PresenterSearchContract = SearchPresenter(createRepository())
     private var totalCount: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        presenter.onAttach(this)
         setUI()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        presenter.onDetach()
     }
 
     private fun setUI() {
